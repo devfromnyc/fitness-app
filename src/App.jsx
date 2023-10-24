0fb9e39055b7a72094afa2bf75553e17e22f08d2
devfromnyc <devfromnyc@gmail.com>
2023-10-24 07:35:11 -0400

    Created and styled navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Home from "./pages/Home";
+import Navbar from "./components/NavBar";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
   );
 }
 
