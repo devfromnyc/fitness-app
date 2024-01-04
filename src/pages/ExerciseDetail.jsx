aa346700c80845378017e8b88648540c55887656
devfromnyc <devfromnyc@gmail.com>
2024-01-03 21:07:40 -0500

    feat: created Exercise Detail Page

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,14 +1,45 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Box } from "@mui/material";
+import Detail from "../components/Detail";
+import ExerciseVideos from "../components/ExerciseVideos";
+import SimilarExercises from "../components/SimilarExercises";
 
-import { exerciseOptions, fetchData } from "../utils/fetchData";
+import { exerciseOptions, fetchData, youtubeOptions } from "../utils/fetchData";
 
 const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseVideos, setExerciseVideos] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = "https://exercisedb.p.rapidapi.com";
+      const youtubeSearchUrl =
+        "https://youtube-search-and-download.p.rapidapi.com";
+
+      const exerciseDetailData = await fetchData(
+        `${exerciseDbUrl}/exercises/exercise/${id}`,
+        exerciseOptions
+      );
+      setExerciseDetail(exerciseDetailData);
+
+      const exerciseVideosData = await fetchData(
+        `${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`,
+        youtubeOptions
+      );
+      setExerciseVideos(exerciseVideosData.contents);
+    };
+    fetchExercisesData();
+  }, [id]);
+
   return (
     <Box>
-      <Detail />
-      <ExerciseVideos />
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos
+        exerciseVideos={exerciseVideos}
+        name={exerciseVideos.name}
+      />
       <SimilarExercises />
     </Box>
   );
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -6,6 +6,15 @@ export const exerciseOptions = {
   },
 };
 
+export const youtubeOptions = {
+  method: "GET",
+  url: "https://youtube-search-and-download.p.rapidapi.com/video/related",
+  headers: {
+    "X-RapidAPI-Key": "34772db147msh3cf91afd00d1cf5p1364c9jsn812f8fab4fe3",
+    "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_KEY,
+  },
+};
+
 export const fetchData = async (url, options) => {
   const response = await fetch(url, options);
   const data = await response.json(response);
